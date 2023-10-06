d321d29a831e5682d4f569bc1d4ba6ade2e15df6
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-10-07 03:57:17 +0600

    structure added

---
+++ src/router/Router.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Root Layout/Root";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Contact from "../Pages/Contact";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+export default router;
