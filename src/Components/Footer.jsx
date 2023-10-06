617374a6d6db0f200b597f4d393d1fb039b0d315
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-10-07 05:50:43 +0600

    footer section added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,31 @@
+import { IoLogoFacebook, IoLogoTwitter, IoLogoYoutube } from "react-icons/io5";
+
 const Footer = () => {
   return (
     <div>
-      <h3>footer</h3>
+      <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+        <nav className="grid grid-flow-col gap-4">
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+        </nav>
+        <nav>
+          <div className="grid grid-flow-col gap-4">
+            <a>
+              <IoLogoFacebook />
+            </a>
+            <a>
+              <IoLogoYoutube />
+            </a>
+            <a>
+              <IoLogoTwitter />
+            </a>
+          </div>
+        </nav>
+        <aside>
+          <p>Copyright © 2023 - All right reserved by Cheers.</p>
+        </aside>
+      </footer>
     </div>
   );
 };
